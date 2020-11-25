eeae0ebe98f8207b4303f91051164b3ff361ce55
Angela Chen <achen10@luc.edu>
2020-11-24 22:00:18 -0500

    adding python script to insert into database

---
+++ insert.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from datetime import date, datetime
+import mysql.connector
+import subprocess
+import re
+
+dbc = mysql.connector.connect(host="127.0.0.1", user="root", password="1234", database="Temperature")
+
+cursor = dbc.cursor()
+
+today = date.today()
+now = datetime.now()
+
+#taking output of bash script and turning it into usable int to add to database
+#bash = subprocess.Popen(['./printTemp.sh'], stdout=subprocess.PIPE)
+bash = subprocess.check_output('./printTemp.sh')
+
+bash = str(bash)
+bashList = bash.split("'")
+
+bash = bashList[1]
+bashList = bash.split("\\")
+bash = bashList[0]
+bash = float(bash)
+
+addEntry = ("INSERT INTO Temperature (Degrees, Time, Date) VALUES (%(bash)s, %(now)s, %(today)s)")
+
+variables = {
+	'bash': bash,
+	'today': today,
+	'now': now,
+}
+
+cursor.execute(addEntry, variables)
+
+dbc.commit()
+cursor.close()
+dbc.close()
